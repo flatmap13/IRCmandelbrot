cd6551ba5350b5613c51a21fe9b0e77bfae8581a
Georgi Khomeriki <g.khomeriki@gmail.com>
2013-05-27 16:06:15 +0200

    lifted socket handle to a monad

--- bot.hs
+++ bot.hs
@@ -4,43 +4,67 @@ import System.IO
 import Text.Printf
 import Data.List
 import System.Exit
+import Control.Arrow
 import Control.Monad
+import Control.Monad.Reader
+import Control.Exception
+import Prelude hiding (catch)
 
 server = "irc.freenode.org"
 port = 6667
 chan = "#tutbot-testing"
 nick = "WOPRbot"
 
+type Net = ReaderT Bot IO
+data Bot = Bot { socket :: Handle }
+
 main :: IO ()
-main = do
+main = bracket connect disconnect loop
+	where
+	disconnect = hClose . socket
+	loop st = catch (runReaderT run st) (\(SomeException _) -> return ())
+
+connect :: IO Bot
+connect = notify $ do
 	h <- connectTo server (PortNumber (fromIntegral port))
 	hSetBuffering h NoBuffering
-	write h "NICK" nick
-	write h "USER" (nick ++ " 0 * :tutorial bot")
-	write h "JOIN" chan
-	listen h
+	return (Bot h)
+	where
+	notify a = bracket_
+		(printf "Connecting to %s...\n" server >> hFlush stdout)
+		(putStrLn "Done.")
+		a
 
-write :: Handle -> String -> String -> IO ()
-write h s t = do
-	hPrintf h "%s %s\r\n" s t
-	printf    "> %s %s\n" s t
+run :: Net ()
+run = do
+	write "NICK" nick
+	write "USER" (nick ++ " 0 * :tutorial bot")
+	write "JOIN" chan
+	asks socket >>= listen
 
-listen :: Handle -> IO ()
+listen :: Handle -> Net ()
 listen h = forever $ do
-	t <- hGetLine h
-	let s = init t
-	if ping s then pong s else eval h (clean s)
-	putStrLn s
+	s <- init `fmap` io (hGetLine h)
+	io (putStrLn s)
+	if ping s then pong s else eval (clean s)
 	where
 	clean     = drop 1 . dropWhile (/= ':') . drop 1
 	ping x    = "PING :" `isPrefixOf` x
-	pong x    = write h "PONG" (':' : drop 6 x) 
+	pong x    = write "PONG" (':' : drop 6 x) 
 
-eval :: Handle -> String -> IO ()
-eval h "!quit" = write h "QUIT" ":Exiting" >> exitSuccess
-eval h x | "!id" `isPrefixOf` x = privmsg h (drop 4 x)
-eval _ _ = return ()
+eval :: String -> Net ()
+eval "!quit" = write "QUIT" ":Exiting" >> io exitSuccess
+eval x | "!id" `isPrefixOf` x = privmsg (drop 4 x)
+eval _ = return ()
 
-privmsg :: Handle -> String -> IO ()
-privmsg h s = write h "PRIVMSG" (chan ++ " :" ++ s)
+privmsg :: String -> Net ()
+privmsg s = write "PRIVMSG" (chan ++ " :" ++ s)
 
+write :: String -> String -> Net ()
+write s t = do
+	h <- asks socket
+	io $ hPrintf h "%s %s\r\n" s t
+	io $ printf    "> %s %s\n" s t
+
+io :: IO a -> Net a
+io = liftIO
