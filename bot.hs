c6e99bcfe20043e843ba1930fb7ccc4c53d576fc
Georgi Khomeriki <g.khomeriki@gmail.com>
2013-06-03 15:29:45 +0200

    added comments

--- bot.hs
+++ bot.hs
@@ -18,15 +18,18 @@ port = 6667
 chan = "#tutbot-testing"
 nick = "WOPRbot"
 
+-- The 'Net' monad, a wrapper over IO, carrying the bot's immutable state.
 type Net = ReaderT Bot IO
 data Bot = Bot { socket :: Handle, startTime :: ClockTime }
 
+-- Set up actions to run on start and end, and run the main loop
 main :: IO ()
 main = bracket connect disconnect loop
     where
     disconnect = hClose . socket
     loop st    = catch (runReaderT run st) (\(SomeException _) -> return ())
 
+-- Connect to the server and return the initial bot state
 connect :: IO Bot
 connect = notify $ do
     t <- getClockTime
@@ -38,6 +41,8 @@ connect = notify $ do
         (printf "Connecting to %s ... " server >> hFlush stdout)
         (putStrLn "done.")
 
+-- We're in the Net monad now, so we've connected successfully
+-- Join a channel, and start processing commands
 run :: Net ()
 run = do
     write "NICK" nick
@@ -45,6 +50,7 @@ run = do
     write "JOIN" chan
     asks socket >>= listen
 
+-- Process each line from the server
 listen :: Handle -> Net ()
 listen h = forever $ do
     s <- init `fmap` io (hGetLine h)
@@ -55,18 +61,23 @@ listen h = forever $ do
     pong x = write "PONG" (':' : drop 6 x) 
     clean  = drop 1 . dropWhile (/= ':') . drop 1
 
+-- Check whether the received message is addressed at the bot
+-- If so, evaluate the message
 maybeEval :: String -> Net ()
 maybeEval s | (nick ++ ": ") `isPrefixOf` s = eval (clean s)
             | otherwise                     = return ()
             where clean = dropWhile (== ' ') . drop 1 . dropWhile (/= ':') 
 
+-- Dispatch a command
 eval :: String -> Net ()
 eval s | s == "uptime"               = uptime >>= privmsg
        | s == "quit"                 = write "QUIT" ":EXITING" >> io exitSuccess
        | "id" `isPrefixOf` s         = privmsg ((dropWhile (== ' ') . drop 2) s)
        | "mandelbrot" `isPrefixOf` s = evalBrot s
-       | otherwise           = return ()
+       | otherwise                   = return ()
 
+-- Evaluates whether the 'mandelbrot' command is valid
+-- If so, the command is dispatched
 evalBrot :: String -> Net ()
 evalBrot s
     | length args /= 2 = privmsg "Usage: mandelbrot width height" 
@@ -75,6 +86,8 @@ evalBrot s
     args = drop 1 $ words s
     size = Size (read $ head args) (read $ args !! 1)
 
+-- Show an (ascii) mandelbrot
+-- Sends the lines with a delay of 1 sec (to prevent flooding)
 showBrot :: Size -> String -> Net ()
 showBrot _ [] = return ()
 showBrot scr @ (Size w _) str = do
@@ -83,21 +96,25 @@ showBrot scr @ (Size w _) str = do
     showBrot scr $ drop w str
     where line = take w str
 
+-- Send a privmsg to the current chan + server
 privmsg :: String -> Net ()
 privmsg s = write "PRIVMSG" (chan ++ " :" ++ s)
 
+-- Send a message out to the server we're currently connected to
 write :: String -> String -> Net ()
 write s t = do
     h <- asks socket
     io $ hPrintf h "%s %s\r\n" s t
     io $ printf    "> %s %s\n" s t
 
+-- Calculate and print the uptime
 uptime :: Net String
 uptime = do
     now <- io getClockTime
     start <- asks startTime
     return . timeDiffToString $ diffClockTimes now start
 
+-- Convenience
 io :: IO a -> Net a
 io = liftIO
 
--- mandelbrot.hs
+++ mandelbrot.hs
@@ -4,19 +4,25 @@ import Data.Char
 
 data Size = Size Int Int
 
+-- Create a String representation of a mandelbrot
+-- with the given size
 createBrot :: Size -> String
 createBrot s @ (Size w h) =
     [ascii $ intensity x y s | x <- [0..w-1], y <- [0..h-1]]
 
+-- Translate a given intensity to an ascii character
 ascii :: Int -> Char
 ascii i = chr $ ord ' ' + (10-i)
 
+-- Given a point (x, y) and a Size returns the
+-- intensity of that point
 intensity :: Int -> Int -> Size -> Int
 intensity x y s @ (Size w h) = iteration 0 0 sx sy 0 10
     where
     sx = -2.5 + 3.5 * fromIntegral x  / fromIntegral w 
     sy = -1 + 2 * fromIntegral y  / fromIntegral h 
 
+-- Recursive iteration to calculate intensity
 iteration :: Float -> Float -> Float -> Float -> Int -> Int -> Int
 iteration x y sx sy i mi
     | x^2 + y^2 < 2*2 && i < mi =
